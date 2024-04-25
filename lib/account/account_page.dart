67667df07ed474679332d11e5fa4ee89704c8f27
TheDarkSide1992 <Jenscp171@gmail.com>
2024-04-25 11:16:08 +0200

    loads data on accont page

--- lib/account/account_page.dart
+++ lib/account/account_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_exam_project/utils/constants.dart';
 
 import '../app_drawer.dart';
+import '../cubit/account/profile_cubit.dart';
+import '../cubit/account/profile_model.dart';
+import '../cubit/account/profile_state.dart';
 
 class AccountPage extends StatelessWidget {
   AccountPage({super.key});
-
-  var firstName = "First Name";
-  var lastName = "Last Name";
-  var mail = "Mail@Mail.mail";
+  var id = 0;
+  var userName = "N/A";
+  var firstName = "N/A";
+  var lastName = "N/A";
+  var mail = "N/A";
+  var city = "N/A";
   var numberOfDevices = 0;
 
   //TODO implement api call refrence to get data. or pull from account state
@@ -20,19 +27,40 @@ class AccountPage extends StatelessWidget {
         title: Text('Account'),
       ),
       drawer: const AppDrawer(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(Icons.account_circle),
-            SizedBox(height: 30),
-            Title(color: Colors.purple, child: Text('Account')),
-            SizedBox(height: 30),
-            Text('First name : $firstName  ,  Last Name: $lastName'),
-            Text('Mail : $mail'),
-            Text('Current number of devices : $numberOfDevices')
-          ],
-        ),
+      body: BlocConsumer<ProfileCubit, ProfileState>(
+        listener: (context, state) {
+          if (state is Autherror || state is ProfileInitial ||state is LoggedOut) {
+            context.showErrorSnackBar(message: "Ann error acoured");
+          }
+        },
+        builder:(context, state) {
+          if (state is LoggedProfile) {
+            Profile profile = state.loggedProfile;
+            id = profile.id;
+            userName = profile.username;
+            firstName = profile.firstName;
+            lastName = profile.lastName;
+            mail = profile.email;
+            city = profile.city;
+          }
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.account_circle),
+                SizedBox(height: 30),
+                Title(color: Colors.purple, child: Text('Account')),
+                SizedBox(height: 30),
+                Text('Username : $userName'),
+                Text('First name : $firstName  ,  Last Name: $lastName'),
+                Text('city : $city'),
+                Text('Mail : $mail'),
+                Text('Current number of devices : $numberOfDevices')
+              ],
+            ),
+          );
+        }
       ),
     );
   }
