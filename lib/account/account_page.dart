7dd2bbe5e931fe328693a978479ed137d4427cf1
TheDarkSide1992 <Jenscp171@gmail.com>
2024-04-25 12:06:55 +0200

    Account page

--- lib/account/account_page.dart
+++ lib/account/account_page.dart
@@ -9,6 +9,7 @@ import '../cubit/account/profile_state.dart';
 
 class AccountPage extends StatelessWidget {
   AccountPage({super.key});
+
   var id = 0;
   var userName = "N/A";
   var firstName = "N/A";
@@ -23,45 +24,49 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text('Account'),
       ),
       drawer: const AppDrawer(),
-      body: BlocConsumer<ProfileCubit, ProfileState>(
-        listener: (context, state) {
-          if (state is Autherror || state is ProfileInitial ||state is LoggedOut) {
-            context.showErrorSnackBar(message: "Ann error acoured");
-          }
-        },
-        builder:(context, state) {
-          if (state is LoggedProfile) {
-            Profile profile = state.loggedProfile;
-            id = profile.id;
-            userName = profile.username;
-            firstName = profile.firstName;
-            lastName = profile.lastName;
-            mail = profile.email;
-            city = profile.city;
-          }
-
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.account_circle),
-                SizedBox(height: 30),
-                Title(color: Colors.purple, child: Text('Account')),
-                SizedBox(height: 30),
-                Text('Username : $userName'),
-                Text('First name : $firstName  ,  Last Name: $lastName'),
-                Text('city : $city'),
-                Text('Mail : $mail'),
-                Text('Current number of devices : $numberOfDevices')
-              ],
-            ),
-          );
+      body:
+      BlocConsumer<ProfileCubit, ProfileState>(listener: (context, state) {
+        if (state is Autherror ||
+            state is ProfileInitial ||
+            state is LoggedOut) {
+          context.showErrorSnackBar(message: "Ann error acoured");
         }
-      ),
+      }, builder: (context, state) {
+        if (state is LoggedProfile) {
+          Profile profile = state.loggedProfile;
+          id = profile.id;
+          userName = profile.username;
+          firstName = profile.firstName;
+          lastName = profile.lastName;
+          mail = profile.email;
+          city = profile.city;
+        }
+
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.account_circle,
+                size: 85,
+              ),
+              Text('$userName',
+                  style: TextStyle(color: Colors.purple, fontSize: 24)),
+              SizedBox(height: 30),
+              Text('First name : $firstName  ,  Last Name: $lastName'),
+              Text('city : $city'),
+              Text('Mail : $mail'),
+              Text('Current number of devices : $numberOfDevices')
+            ],),
+        );
+      }),
     );
   }
 }
